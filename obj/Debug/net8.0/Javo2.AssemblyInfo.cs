776b2e2bb0aa1c1530f877d92e7dc56d5360990c
Alan Miñana <alanminana@gmail.com>
2025-05-01 01:23:54 -0300

    Primer commit5

--- obj/Debug/net8.0/Javo2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Javo2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Javo2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cfefcfea5fb19cce14e254ca8cd73949311a514")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a47d62312665c017ff129ab1ed35269a5d0048")]
 [assembly: System.Reflection.AssemblyProductAttribute("Javo2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Javo2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
